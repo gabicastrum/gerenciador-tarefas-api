87125ce923fbeef8f960e63280bf09d420160d22
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-18 14:50:54 -0300

    feat: criado repositorio de tarefa

---
+++ src/main/java/com/gerenciador/tarefas/repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package com.gerenciador.tarefas.repository;
+
+import com.gerenciador.tarefas.domain.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
