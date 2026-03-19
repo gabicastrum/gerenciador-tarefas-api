15918c55db755aba4c8d0b5d57c92934872f8cdc
Gabriela de Castro Laurindo <gabriela.laurindo@dbserver.com.br>
2026-03-19 11:31:15 -0300

    test: implementado testes para serviço de listar tarefas e refatorado

--- src/test/java/com/gerenciador/tarefas/service/TarefaServiceTest.java
+++ src/test/java/com/gerenciador/tarefas/service/TarefaServiceTest.java
@@ -3,7 +3,9 @@ package com.gerenciador.tarefas.service;
 import com.gerenciador.tarefas.domain.StatusTarefa;
 import com.gerenciador.tarefas.domain.Tarefa;
 import com.gerenciador.tarefas.dtos.request.TarefaRequestDTO;
+import com.gerenciador.tarefas.dtos.response.PageResponseDTO;
 import com.gerenciador.tarefas.dtos.response.TarefaResponseDTO;
+import com.gerenciador.tarefas.exception.TarefaException;
 import com.gerenciador.tarefas.mapper.TarefaMapper;
 import com.gerenciador.tarefas.repository.TarefaRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -13,8 +15,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -26,6 +33,10 @@ public class TarefaServiceTest {
     private static final String TITULO_INICIAL = "Estudar testes unitarios";
     private static final String DESCRICAO_INICIAL = "Aprender JUnit 5";
     private static final Long ID_01 = 1L;
+
+    private static final String ERRO_LISTAGEM = "Erro ao listar tarefas";
+    private static final String ERRO_BANCO_SIMULADO = "Erro SQL";
+
 
     @InjectMocks
     private TarefaService tarefaService;
@@ -41,9 +52,9 @@ public class TarefaServiceTest {
     class CriarTarefa {
         @Test
         @DisplayName("Deve criar uma tarefa com sucesso")
-        void deveCriarUmaTarefaComSucesso(){
-            var requestDTO = new TarefaRequestDTO(TITULO_INICIAL,DESCRICAO_INICIAL);
-            var tarefaEntity = new Tarefa(TITULO_INICIAL,DESCRICAO_INICIAL);
+        void deveCriarUmaTarefaComSucesso() {
+            var requestDTO = new TarefaRequestDTO(TITULO_INICIAL, DESCRICAO_INICIAL);
+            var tarefaEntity = new Tarefa(TITULO_INICIAL, DESCRICAO_INICIAL);
             var responseDTO = criarTarefaResponseDTO();
 
             when(tarefaMapper.toEntity(requestDTO)).thenReturn(tarefaEntity);
@@ -68,16 +79,62 @@ public class TarefaServiceTest {
             var tarefaEntity = new Tarefa(TITULO_INICIAL, DESCRICAO_INICIAL);
 
             when(tarefaMapper.toEntity(requestDTO)).thenReturn(tarefaEntity);
-            when(tarefaRepository.save(any(Tarefa.class))).thenThrow(new RuntimeException("Erro ao salvar"));
 
-            assertThrows(RuntimeException.class, () -> {
+            when(tarefaRepository.save(any(Tarefa.class))).thenThrow(new RuntimeException(ERRO_BANCO_SIMULADO));
+
+            assertThrows(TarefaException.class, () -> {
                 tarefaService.criarTarefa(requestDTO);
             });
 
-            verify(tarefaMapper).toEntity(requestDTO);
             verify(tarefaRepository).save(tarefaEntity);
-
             verify(tarefaMapper, never()).toDto(any());
+        }
+
+        @Nested
+        @DisplayName("listarTarefas")
+        class ListarTarefa {
+            @Test
+            @DisplayName("Deve listar tarefas paginadas com sucesso")
+            void deveListarTarefasComSucesso() {
+                Pageable pageable = PageRequest.of(0, 10);
+
+                var tarefaEntity = new Tarefa(TITULO_INICIAL, DESCRICAO_INICIAL);
+                var tarefaResponse = criarTarefaResponseDTO();
+
+                Page<Tarefa> page = new PageImpl<>(List.of(tarefaEntity), pageable, 1);
+
+                when(tarefaRepository.findAll(pageable)).thenReturn(page);
+                when(tarefaMapper.toDto(tarefaEntity)).thenReturn(tarefaResponse);
+
+                PageResponseDTO<TarefaResponseDTO> resultado =
+                        tarefaService.listarTarefas(pageable);
+
+                assertNotNull(resultado);
+                assertEquals(1, resultado.conteudo().size());
+                assertEquals(TITULO_INICIAL, resultado.conteudo().get(0).titulo());
+                assertEquals(0, resultado.pagina());
+                assertEquals(10, resultado.tamanho());
+
+                verify(tarefaRepository).findAll(pageable);
+                verify(tarefaMapper).toDto(tarefaEntity);
+            }
+
+            @Test
+            @DisplayName("Deve lançar exceção ao falhar ao listar tarefas")
+            void deveLancarExcecaoAoListarTarefas() {
+                Pageable pageable = PageRequest.of(0, 10);
+
+                when(tarefaRepository.findAll(pageable))
+                        .thenThrow(new TarefaException(ERRO_LISTAGEM));
+
+                assertThrows(TarefaException.class, () -> {
+                    tarefaService.listarTarefas(pageable);
+                });
+
+                verify(tarefaRepository).findAll(pageable);
+
+                verify(tarefaMapper, never()).toDto(any());
+            }
         }
 
         private TarefaResponseDTO criarTarefaResponseDTO() {
